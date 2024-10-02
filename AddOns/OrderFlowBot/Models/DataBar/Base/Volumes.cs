e7d763a48563999911efaa7e56a25aae3b62e0c9
WaleeTheRobot <d2fsbgv5@gmail.com>
2024-10-02 10:30:52 -0400

    Added value area high and low price

--- AddOns/OrderFlowBot/Models/DataBar/Base/Volumes.cs
+++ AddOns/OrderFlowBot/Models/DataBar/Base/Volumes.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using NinjaTrader.Custom.AddOns.OrderFlowBot.DataBarConfigs;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NinjaTrader.Custom.AddOns.OrderFlowBot.Models.DataBar.Base
 {
@@ -18,12 +20,14 @@ namespace NinjaTrader.Custom.AddOns.OrderFlowBot.Models.DataBar.Base
         public long BuyingVolume { get; set; }
         public long SellingVolume { get; set; }
         public List<BidAskVolume> BidAskVolumes { get; set; }
+        public double ValueAreaPercentage { get; set; }
         public double ValueAreaHighPrice { get; set; }
         public double ValueAreaLowPrice { get; set; }
 
         public Volumes()
         {
             BidAskVolumes = new List<BidAskVolume>();
+            ValueAreaPercentage = DataBarConfig.Instance.ValueAreaPercentage * 0.01;
         }
 
         public void SetBidAskPriceVolumeAndVolumeDelta()
@@ -40,6 +44,47 @@ namespace NinjaTrader.Custom.AddOns.OrderFlowBot.Models.DataBar.Base
                 bidAskVolume.VolumeDelta = bidAskVolume.AskVolume - bidAskVolume.BidVolume;
                 BidAskVolumes[i] = bidAskVolume;
             }
+        }
+
+        // This can be updated to the one NinjaTrader provides if it ever gets created
+        public void SetValueArea()
+        {
+            if (BidAskVolumes == null || BidAskVolumes.Count == 0)
+            {
+                return;
+            }
+
+            var sortedVolumes = BidAskVolumes.OrderBy(v => v.Price).ToList();
+
+            var poc = sortedVolumes.OrderByDescending(v => v.Volume).First();
+            PointOfControl = poc.Price;
+
+            long totalVolume = sortedVolumes.Sum(v => v.Volume);
+            long valueAreaVolume = (long)(totalVolume * ValueAreaPercentage);
+            long currentVolume = poc.Volume;
+
+            int lowerIndex = sortedVolumes.IndexOf(poc);
+            int upperIndex = lowerIndex;
+
+            while (currentVolume < valueAreaVolume && (lowerIndex > 0 || upperIndex < sortedVolumes.Count - 1))
+            {
+                long lowerVolume = (lowerIndex > 0) ? sortedVolumes[lowerIndex - 1].Volume : 0;
+                long upperVolume = (upperIndex < sortedVolumes.Count - 1) ? sortedVolumes[upperIndex + 1].Volume : 0;
+
+                if (lowerVolume >= upperVolume)
+                {
+                    currentVolume += lowerVolume;
+                    lowerIndex--;
+                }
+                else
+                {
+                    currentVolume += upperVolume;
+                    upperIndex++;
+                }
+            }
+
+            ValueAreaLowPrice = sortedVolumes[lowerIndex].Price;
+            ValueAreaHighPrice = sortedVolumes[upperIndex].Price;
         }
     }
 }
